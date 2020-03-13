c22e94a3e325eb737151cf31d27bfe0fdbc845e8
SunnyBarua <sunnybarua95@gmail.com>
2020-03-13 17:25:15 +0600

    Initialize project using Create React App

---
+++ src/fakeData/users.js
@@ -0,0 +1,19 @@
+var users =
+    [{"id": 1,"name":"Jason Statham","position":"Web Developer","sallery":"$6000","img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTKbC7qWS0G0rqflHgXA9XymZ9gzbwfkpzs0L9ZBrRrCcDYuysy", "email": "abc@april.biz"},
+     {"id": 2,"name":"Jason Roy","position":"Mobile App Developer","sallery":"$5000", "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTmcHvwLhYI6JURsvouMef6dsO2KUJrDdt962dHs5o8Z4YLLs5t", "email":"bcd@april.biz"},
+     {"id": 3,"name":"Steven Smith","position":"Application Architecture","sallery":"$5500", "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSOdaBPMQNyEVGX3vv8cF-jR0Bmz54-g5I5qo6aruP6UPps6lea", "email": "zyr@april.biz"},
+     {"id": 4,"name":"Hiallari Roy","position":"React Developer","sallery":"$2500", "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSgMN6JxrgM7NxPMBkKRxN0pJGedX9hjomdpsX9YzSBniB7MT2z", "email": "ewre@april.biz"},
+     {"id": 5,"name":"Mark Taylor","position":"Vue Developer","sallery":"$3500", "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTrDMhtgPgK9W0WrMdAyz25-MiHdG8K7LBY_tR1kVv_cRrLUQDE" ,"email": "hrht@april.biz"},
+     {"id": 6,"name":"David Malan","position":"Python Developer","sallery":"$7500", "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTDcHJDwyeqU7rMDxqcyk19Ys_g95UQR1aDkzukFzMxFtszIni9", "email": "ehthr@april.biz"},
+     {"id": 7,"name":"Stuart Broad","position":"Java Developer","sallery":"$4700", "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSaTmMTP74WBiJNKw4_I16x_qySJfqJL61B7pL0CdBFlSjiaRlh" ,"email": "rtew@april.biz",},
+     {"id": 8,"name":"James Anderson","position":"Wordpress Developer","sallery":"$4500", "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRuju8Aq9_MA22kV8Cm-wWuLEQuWQwUr5ejIew3o0_CbuAn8GCe" ,"email": "gewy@april.biz"},
+     {"id": 9,"name":"Maxwell","position":"Graphics Designer","sallery":"$4600", "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQQOwoX4ta-Zr80blRV5-It94dMtpXzmP2rMV7v2lKG3DNyX14f", "email": "tyrjyt@april.biz"},
+     {"id": 10,"name":"AB Devilliers","position":"Project Manager","sallery":"$5500", "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSbsIqQ0YiPtyDolGeVgmikeQdaT9QOawC7n-P0DQm_v__KdvdI", "email": "ehrw@april.biz"},
+     {"id": 11,"name":"Graeme Smith","position":"UI Designer","sallery":"$4500", "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjDl1ItaJMoyYJdnwfQNo2p5LQhj-fqNUiPvaLjOzfiaBx35Y3" ,"email": "rht@april.biz"},
+     {"id": 12,"name":"Piccani","position":"Quality Manager","sallery":"$4500", "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQnOOr0c3JOBKiCurnH2dTR_AuJRDNHlNSNa9VW7Os3fMR85iz", "email": "wrhae@april.biz"},
+     {"id": 13,"name":"Bital Xoyee","position":"Tester","sallery":"$4500", "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQztoMILUBfnbcsY6p58G_9_2nTCKej0xJDl8daoeEqgC89IP84", "email": "whare@april.biz"},
+     {"id": 14,"name":"Thomas Alba","position":"Trainer","sallery":"$5000", "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTPd5KaBWzNo-T-0i4r2q-Dx60mnt7QcdFA5QuDWavAg7pDFY8x", "email": "yej@april.biz"},
+     {"id": 15,"name":"Mark Taylor","position":"Marketer","sallery":"$2500", "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ1JTGJu69BFcSu-bW7mRK_Sd_fGQrnmOAwR7vsE4ztLa9dcpUf", "email": "ehuj@april.biz"},
+
+]
+export default users;
